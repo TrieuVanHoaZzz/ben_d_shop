6f22396316c97a7adb06080c21316acd399a72de
TrieuVanHoaZzz <dongtriutoichoi@gmail.com>
2025-11-14 14:33:13 +0700

    slider

--- src/component/search.jsx
+++ src/component/search.jsx
@@ -2,16 +2,16 @@ import { FaSearch } from "react-icons/fa";
 
 function Search() {
     return (
-<div className="">
-    <section className="flex items-center bg-white py-1 px-2 rounded-md gap-1 ">
-        <span className=" inline-block font-light">
-            <FaSearch />
-        </span>
-        <input type="text" placeholder="Tìm kiếm sản phẩm" className=" focus:outline-0" />
-        <button className=" cursor-pointer text-amber-500 ms-10 hidden sm:block">Tìm kiếm</button>
-    </section>
+        <div className="">
+            <section className="flex items-center bg-white py-1 px-1 rounded-md gap-1 ">
+                <span className=" inline-block font-light">
+                    <FaSearch />
+                </span>
+                <input type="text" placeholder="Tìm kiếm " className=" focus:outline-0" />
+                <button className=" cursor-pointer text-amber-500 ms-10 hidden sm:block">Tìm kiếm</button>
+            </section>
 
-</div>
+        </div>
     );
 }
 export default Search;
--- src/component/trangchu.jsx
+++ src/component/trangchu.jsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../index.css'
-import Test from './test.jsx'
 import Header from './header.jsx'
 import Nav from './nav.jsx'
+import Banner from './banner.jsx'
+import Product_Card from './product_card.jsx'
+import Product_Limit from './product_limit.jsx'
+import Footer from './footer.jsx'
  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
-      <Header />
-      <Nav></Nav>
+  <Header></Header>
+    <Nav></Nav>
+  <Banner></Banner>
+  <Product_Limit></Product_Limit>
+  <Footer></Footer>
 
   </StrictMode>,
 )
